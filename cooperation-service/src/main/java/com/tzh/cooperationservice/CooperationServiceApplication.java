b16181bae2717239fc2c55d227290266cd851dec
tzh <tangzihao@baiseng.com>
2018-11-07 17:55:39 +0800

    配置链路追踪zipkin

--- cooperation-service/src/main/java/com/tzh/cooperationservice/CooperationServiceApplication.java
+++ cooperation-service/src/main/java/com/tzh/cooperationservice/CooperationServiceApplication.java
@@ -1,11 +1,13 @@
 package com.tzh.cooperationservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author tzh
@@ -18,5 +20,10 @@ public class CooperationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CooperationServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
--- jurisdiction-service/src/main/java/com/tzh/jurisdictionservice/JurisdictionServiceApplication.java
+++ jurisdiction-service/src/main/java/com/tzh/jurisdictionservice/JurisdictionServiceApplication.java
@@ -1,10 +1,12 @@
 package com.tzh.jurisdictionservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author tzh
@@ -17,5 +19,10 @@ public class JurisdictionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JurisdictionServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
